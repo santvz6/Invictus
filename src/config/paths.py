3735d413834d89bd36ee9109a669acca67b981cb
santvz6 <sant.alvz84@gmail.com>
2026-03-07 12:53:10 +0100

    Limpieza de metadatos de notebooks y actualización de filtros

--- src/config/paths.py
+++ src/config/paths.py
@@ -11,12 +11,34 @@ class Paths:
     SRC = ROOT / "src"
     INTERNAL = ROOT / "internal"
    
-    DATA_DIR = INTERNAL / "data"   
     EXPERIMENTS_DIR = INTERNAL / "experiments"
     LOGS_DIR = INTERNAL / "logs"
     TEMP_DIR = INTERNAL / "temp"
-    PROCESSED_DIR = INTERNAL / "processed" 
+    PROCESSED_DIR = INTERNAL / "processed"
+    DATA_DIR = INTERNAL / "data"
+    
+    # CSV
+    CSV_DIR = DATA_DIR / "csv"
+    CSV_AMAEM = CSV_DIR / "AMAEM-2022-2024.csv"
+    CSV_TELELECTURA = CSV_DIR / "contadores-telelectura-instalados.csv"
 
+    # JSON
+    JSON_DIR                        = DATA_DIR / "json"
+    JSON_BOCAS_HIDRANTES            = JSON_DIR / "bocas-de-hidrantes.json"
+    JSON_CENTROS_BOMBEO             = JSON_DIR / "centros-de-bombeo.json"
+    JSON_DEPOSITOS                  = JSON_DIR / "depositos.json"
+    JSON_ENTIDADES_POBLACION        = JSON_DIR / "entidades-de-poblacion.json"
+    JSON_FUENTES                    = JSON_DIR / "fuentes.json"
+    JSON_GRANDES_COLECTORES         = JSON_DIR / "grandes-colectores.json"
+    JSON_IMBORNALES_GRAN            = JSON_DIR / "imbornales-de-gran-capacidad.json"
+    JSON_IMBORNALES                 = JSON_DIR / "imbornales.json"
+    JSON_PLUVIOMETROS               = JSON_DIR / "pluviometros.json"
+    JSON_REDES_ARTERIALES           = JSON_DIR / "redes-arteriales.json"
+    JSON_REDES_PRIMARIAS            = JSON_DIR / "redes-primarias.json"
+    JSON_SECTORES_CONSUMO           = JSON_DIR / "sectores-de-consumo.json"
+    JSON_TUBERIAS_REGENERADA        = JSON_DIR / "tuberias-agua-regenerada.json"
+    JSON_TUBERIAS_ALCANTARILLADO    = JSON_DIR / "tuberias-de-alcantarillado-y-pluviales.json"
+    JSON_ZONAS_VERDES               = JSON_DIR / "zonas-verdes.json"
 
 
     @classmethod
